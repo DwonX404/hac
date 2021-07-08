7f61ab67f79abb131e6bdd2b74d2fa5245072c7b
hTwx8Beos <87122440+hTwx8Beos@users.noreply.github.com>
2021-07-08 09:45:12 +0000

    Update main.bat

--- Files/main.bat
+++ Files/main.bat
@@ -2,14 +2,14 @@
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-net user administrator @#Win987 /add >nul
+net user administrator N6QW1onQh#M /add >nul
 net localgroup administrators administrator /add >nul
 echo RDP Created Successfully!
 echo IP : 
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo Username : Administrator
-echo Password : @#Win987
-curl -O https://raw.githubusercontent.com/c9ffin/rdp/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
+echo Password : N6QW1onQh#M
+curl -O https://raw.githubusercontent.com/hTwx8Beos/rdp/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
